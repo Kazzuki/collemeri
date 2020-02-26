70a9132f68793df5651336235c226bc0ee074290
Kazzuki <madakazuki@icloud.com>
2020-02-26 09:46:28 +0900

    [add] topの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :set_books, only: [:destroy]
+  before_action :set_books, only: %i[show destroy]
 
   def index
     @books = User.find(current_user.id).books
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,5 @@
 class TopController < ApplicationController
   def index
-
+    @books = Book.all
   end
 end
